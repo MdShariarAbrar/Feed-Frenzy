2efcecc5b6c849d07b24f18a1d439a22c9e55094
MdShariarAbrar <shariara@outlook.com>
2024-03-04 14:34:39 +0600

    All post section dynamically update complete

--- js/index.js
+++ js/index.js
@@ -9,33 +9,35 @@ async function loadAllPost() {
 
 
 const displayAllPosts = posts => {
-    posts.array.forEach(post => {
+
+    const postContainer = document.getElementById("all-post-container")
+    posts.forEach(post => {
 
        postCard = document.createElement('div');
        postCard.innerHTML = `
-       <div class="rounded-3xl bg-[#F3F3F5] p-4 lg:p-10 flex gap-6 flex-col lg:flex-row">
+       <div class="rounded-3xl bg-[#F3F3F5] p-4 lg:p-10 flex gap-6 flex-col lg:flex-row mb-6">
 
        <div>
-         <div class="avatar online">
+         <div class="avatar ${post.isActive ? 'online' : 'offline'}">
            <div class="w-24 rounded-full">
              <img src=${post.image} />
            </div>
          </div>
        </div>
 
-       <div>
+       <div class="flex-1">
          <div class="text-[#12132DCC] text-base font-medium">
-           <span># Music</span> 
-           <span>Author: Awlad Hossain</span>
+           <span># ${post.category}</span> 
+           <span>Author: ${post.author.name}</span>
          </div>
-         <h2 class="text-[#12132D0] text-xl font-bold mb-6">10 Kids Unaware of Their Halloween Costume</h2>
-         <p class="text-[#12132D99] text-base ">It’s one thing to subject yourself to ha Halloween costume mishap because, hey that’s your prerogative</p>
+         <h2 class="text-[#12132D0] text-xl font-bold mb-6">${post.title}</h2>
+         <p class="text-[#12132D99] text-base ">${post.description}</p>
          <hr class="border border-dashed my-6 border-[#12132D99]">
-         <div class="flex justify-between items-center ">
+         <div class="flex justify-between items-center w-full">
            <div class="text-[#12132D99] flex gap-4">
-             <span><i class="fa-regular fa-message"></i> 560</span>
-             <span><i class="fa-regular fa-eye"></i> 1568</span>
-             <span><i class="fa-regular fa-clock"></i> 5 min</span>
+             <span><i class="fa-regular fa-message"></i> ${post.comment_count}</span>
+             <span><i class="fa-regular fa-eye"></i> ${post.view_count}</span>
+             <span><i class="fa-regular fa-clock"></i> ${post.posted_time} min</span>
            </div>
            <div class="bg-[#10B981] rounded-full w-7 h-7 text-center text-white">
              <i class="fa-solid fa-envelope-open"></i>
@@ -44,7 +46,7 @@ const displayAllPosts = posts => {
        </div>
      </div>
        `
-
+        postContainer.appendChild(postCard);
     });
 }
 
